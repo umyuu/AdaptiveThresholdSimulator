26a0e8246e3626f1f7cb9ef779654ce99c56a3e3
umyuu <124dtiaka@gmail.com>
2018-07-15 03:05:31 +0900

    visualizer

--- pref/profiling.bat
+++ pref/profiling.bat
@@ -1,5 +1,5 @@
 cd ..\src
 
 python -m cProfile -o ..\pref\program.prof adaptiveThreshold.py
-python C:\Users\%USERNAME%\AppData\Roaming\Python\Python36\Scripts\snakeviz.exe ..\pref\program.prof
+CALL ..\pref\visualizer.BAT
 TIMEOUT 30
